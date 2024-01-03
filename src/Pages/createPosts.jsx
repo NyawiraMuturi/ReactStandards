75521a7e2ca7df9e4d8c860fdea4aee6f6d19ba3
NyawiraMuturi <albinamuturi@gmail.com>
2024-01-03 20:18:46 +0300

    introduce useMutation hook

--- src/Pages/createPosts.jsx
+++ src/Pages/createPosts.jsx
@@ -1,77 +1,43 @@
 import React, {useState} from 'react'
 import { useForm } from 'react-hook-form'
-import {FormErrorMessage,FormLabel,FormControl,Input,Button, Textarea} from '@chakra-ui/react'
+import {Input,Button, Textarea, Text} from '@chakra-ui/react'
 import { useMutation } from '@tanstack/react-query'
 import axiosClient from '../api/axios'
 
 const CreatePosts = () => {
-  const [title, setTitle] = useState('')
-  const [descrition, setDescription] = useState('')
-
   const {
-    handleSubmit,
     register,
-    formState: { errors, isSubmitting },
+    formState: { errors },
+    handleSubmit,
   } = useForm()
 
-  const { isLoading, isError, error, mutate } = useMutation(createPost, {
-    retry: 3,
-  });
-
-  const createPost = async ()=>{
-    try {
-      const response = await axiosClient.post('/posts')
-      return response.data
-
-    } catch (error) {
-
-    }
-  }
-  function onSubmit(values) {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        alert(JSON.stringify(values, null, 2))
-        resolve()
-      }, 3000)
-    })
-  }
-
+const onSubmit =()=>{
+  alert('Form Submitted')
+}
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <FormControl isInvalid={errors.title && errors.description}>
-        <FormLabel htmlFor='title'>Add New Post Here</FormLabel>
-        <Input
-          id='title'
-          placeholder='title'
-          {...register('title', {
-            required: 'This field is required',
-            minLength: { value: 4, message: 'Minimum length should be 4' },
-          })}
-        />
-        <FormErrorMessage>
-          {errors.title && errors.title.message}
-        
-        </FormErrorMessage>
-   
-        <Textarea
-          mt={4}
-          id='description'
-          placeholder='description'
-          {...register('description', {
-            required: 'This field is required',
-            
-          })}
-        />
+      <label htmlFor="">Title</label>
+      <Input
+      mb={4}
+        placeholder='Title'
+        {...register("newPost", { required: true })}
+        aria-invalid={errors.newPost ? "true" : "false"}
+      />
+      {errors.newPost?.type === "required" && (
+        <Text color='tomato'>Title cannot be blank</Text>
+      )}
 
-        <FormErrorMessage>
+        <label htmlFor="">Description</label>
+      <Textarea
+      placeholder='Description'
+      
+        {...register("description", { required: "Description is required" })}
+        aria-invalid={errors.description ? "true" : "false"}
+      />
+      {errors.description && <Text color='tomato'>{errors.description.message}</Text>}
 
-          {errors.description && errors.description.message}
-        </FormErrorMessage>
-
-      </FormControl>
-      <Button my={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
-        Add Post
-      </Button>
+      <Button 
+      type='submit' my={4}>Add post</Button>
     </form>
   )
 }
