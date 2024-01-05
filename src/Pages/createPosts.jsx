68f2d9837acc0b9501e08a3ae87eb7978f184141
NyawiraMuturi <albinamuturi@gmail.com>
2024-01-05 19:45:15 +0300

    handle form change onSubmit

--- src/Pages/createPosts.jsx
+++ src/Pages/createPosts.jsx
@@ -1,68 +1,52 @@
 import React, {useState} from 'react'
-import { useForm } from 'react-hook-form'
-import {Input,Button, Textarea, Text, FormControl} from '@chakra-ui/react'
+import {Input,Button, Textarea, Text, FormControl, FormHelperText, FormErrorMessage, FormLabel} from '@chakra-ui/react'
 import { useMutation } from '@tanstack/react-query'
 import axiosClient from '../api/axios'
 import { useAddPost } from '../hooks/usePostDetails'
 
 const CreatePosts = () => {
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-} = useForm()
-
-
-
 
   const [title, setTitle]= useState('')
   const [body, setBody]= useState('')
 
-
   const {mutate}=useAddPost()
 
-  const onSubmit = ()=>{
+  const onSubmit = (e)=>{
+    e.preventDefault();
     const newPost = {title, body}
     mutate(newPost)
     console.log({title, body})
+
+    setTitle('')
+    setBody('')
   }
   return (
 
     <>
 
-  <form onSubmit={handleSubmit(onSubmit)}>
-      <FormControl>
-        <label htmlFor="">Title</label>
+  <form onSubmit={onSubmit}>
+
+    <FormControl isRequired>
+        <FormLabel>Add Post</FormLabel>
         <Input
-        
-        mb={4}
-          // value={title}
-          onChange={(e)=> {
-            title.onChange(e);
-            setTitle(e)
-          }}
-          placeholder='Title'
-          {...register("title", { required: true })}
-          aria-invalid={errors.title ? "true" : "false"}
+        type='text'
+        value={title}
+        onChange={(e)=>setTitle(e.target.value)}
         />
-        {errors.title?.type === "required" && (
-          <Text color='tomato'>Title cannot be blank</Text>
-        )}
+       
+    </FormControl>
 
-          <label htmlFor="">Description</label>
+    <FormControl isRequired>
+        <FormLabel>Description</FormLabel>
         <Textarea
-          // value={body}
-          onChange={(e)=>setBody(e.target.value)}
-        placeholder='Description'
-        
-          {...register("body", { required: "Description is required" })}
-          aria-invalid={errors.body ? "true" : "false"}
+        type='text'
+        value={body}
+        onChange={(e)=>setBody(e.target.value)}
         />
-        {errors.body && <Text color='tomato'>{errors.body.message}</Text>}
-      </FormControl>
+        
+    </FormControl>
 
-
-      <Button type='submit' my={4}>Add post</Button>
+      <Button type='submit' my={4} colorScheme='teal'>Add post</Button>
     </form>
 
    
